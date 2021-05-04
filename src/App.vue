3120da24bf3d2b0b3c130b1a115c91f663143b8f
jasmine <18674601207@163.com>
2021-05-05 04:35:38 +0800

    add global css

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,7 @@ export default {
 </script>
 
 <style>
+/* 全局样式 */
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +25,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+/* 自定义图标的样式，在网站上下载至本地，打开demo_index.html可以找到symbol代码自定义的样式 */
+.icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
 }
 </style>
